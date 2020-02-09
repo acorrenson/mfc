c1f1c89ae25b0d838ab29e405e20aae578739a05
Nathan Graule <solarliner@gmail.com>
2020-02-09 21:36:21 +0100

    Make parse_or take 'a t -> 'a t instead of ('a t * 'a t)

--- lib/mfc_parsing.mli
+++ lib/mfc_parsing.mli
@@ -8,16 +8,16 @@ val pmap : 'a -> 'b -> 'b parse_result
 val pand : 'a -> 'b parse_result -> 'b parse_result
 val parse_char : char -> 'a t
 
-val parse_or : ('a t) * ('a t) -> 'a t
+val parse_or : 'a t -> 'a t -> 'a t
 val parse_any : 'a t list -> 'a list t
 val parse_all : 'a t list -> 'a list t
 val parse_many : 'a t -> 'a list t
-val parse_concat_many : string parser -> string parser
-val parse_ignore : 'a parser -> 'b parser -> 'b parser
-val parse_skip : 'a parser -> 'b parser -> 'b parser
+val parse_concat_many : string t -> string t
+val parse_ignore : 'a t -> 'b t -> 'b t
+val parse_skip : 'a t -> 'b t -> 'b t
 val parse_concat_seq : (string t) list -> string t
 val parse_combine_seq : ('a t) -> 'a t
-val parse_literal : string -> string parser
-val parse_anychar_in : string -> string parser
-val parse_delim : 'a parser -> 'b parser -> 'b parser
-val parse_wrap : 'a parser -> 'b parser -> 'c parser -> 'c parser
+val parse_literal : string -> string t
+val parse_anychar_in : string -> string t
+val parse_delim : 'a t -> 'b t -> 'b t
+val parse_wrap : 'a t -> 'b t -> 'c t -> 'c t
