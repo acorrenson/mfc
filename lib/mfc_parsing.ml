ba39e61c0d2964884fc0af5d516548937e79bb51
Arthur Correnson <arthur.correnson@gmail.com>
2020-02-10 16:58:33 +0100

    Add docstring in parsing module

--- lib/mfc_parsing.ml
+++ lib/mfc_parsing.ml
@@ -1,52 +1,89 @@
+(** Parsing data *)
 type 'a pdata = ('a * string)
+
+(** Parsing result (monadic) *)
 type 'a presult = 'a pdata option
+
+(** Parser constructor *)
 type 'a parser = P of (string -> 'a presult)
 
 exception ParseException of string
 
-let pzero = P(fun _ -> None)
-let pone = P(fun rest -> Some([], rest))
+(**
+   Null parser.
+   [parse pszero s] always returns None
+*)
+let pzero = P (fun _ -> None)
 
+(**
+   Empty parser.
+   [parse pone s] always returns [Some ([], s)].
+*)
+let pone = P (fun rest -> Some([], rest))
+
+(**
+   [parse p inp] applies the parser [p] to the input string [inp].
+*)
 let parse p inp =
   match p with
   | P (p) -> p inp
 
+(**
+   [pmap f (parse p inp)] applies the function f 
+   to result of [parse p inp] application.
+*)
 let pmap f: 'a presult -> 'b presult =
   function
   | Some(c, out) -> Some(f c, out)
   | None -> None
 
+(** Try to get the result of a parser, raises [ParseException] *)
 let pleak: 'a presult -> 'a =
   function
   | Some(c, _) -> c
   | None -> raise (ParseException "Attempted to get failed parse")
 
+(** [parse (fmap f p) inp] is [parse p inp |> pmap f] *)
 let fmap f p =
   P (fun inp -> parse p inp |> pmap f)
 
+(**
+   Binding operator.
+   [parse (p >>= f) inp] is [parse (f r) out] if [parse p inp] retunrs [Some (r, out)].
+*)
 let (>>=) p f =
   P (fun inp -> match parse p inp with
       | None -> None
       | Some(v, out) -> parse (f v) out)
 
+(**
+   Or operator.
+   [p <|> q] accepts all strings accepted by [p] or by [q].
+*)
 let (<|>) p q =
   P (fun inp -> match parse p inp with
       | None -> parse q inp
       | _ as r -> r)
 
+(**
+   Syntax sugar for bindings.
+   [let* x = p in f x] is [p >>= (fun x -> f x)] where f returns a parser.
+*)
 let (let*) x f = x >>= f
 
+(**
+   Parser for a single char [c].
+*)
 let pchar c =
   P (fun inp -> match inp with
       | "" -> None
       | x when x.[0] = c -> Some(c, String.sub x 1 ((String.length x) - 1))
       | _ -> None)
 
-let p =
-  let* x = pchar 'c' in
-  let* y = pchar 'd' in
-  P (fun inp -> Some ((x, y), inp))
-
+(**
+   The parser [some p] tries to apply [p] zero or more times.
+   Retunrs the result as a list.
+*)
 let rec some (p : 'a parser) =
   P (fun inp ->
       match parse p inp with
@@ -57,6 +94,10 @@ let rec some (p : 'a parser) =
         | None -> None
     )
 
+(**
+   The parser [some p] tries to apply [p] one or more times.
+   Returns the result as a list (or None).
+*)
 let many (p : 'a parser) =
   let* x = p in
   let* lx = some p in
@@ -64,41 +105,55 @@ let many (p : 'a parser) =
       Some (x::lx, inp)
     )
 
+(**
+   [anychar_of cl] accepts any char in the list [cl].
+*)
 let anychar_of cl =
   match cl with
   | x::rest -> List.fold_left (<|>) (pchar x) (List.map pchar rest)
   | [] -> P (fun _ -> None)
 
+(**
+   Convert a string into a list of char.
+*)
 let explode s =
   let rec step lc i =
     if i = String.length s then lc
     else step (lc @ [s.[i]]) (i+1)
   in
   step [] 0
-let rec combine: char list -> string =
-  function
-  | [] -> ""
-  | c::cl -> (String.make 1 c) ^ (combine cl)
-let rec combine_str: string list -> string =
-  function
-  | [] -> ""
-  | s::sl -> s ^ (combine_str sl)
 
+(**
+   [anychar_in s] accepts any char in string [s].
+*)
 let anychar_in s =
   explode s |> anychar_of
 
-let rec sequence: ('a parser list -> 'a list parser) =
+(**
+   Convert a list of chars into a string.
+*)
+let combine_str ls = List.fold_left (^) "" ls
+
+(**
+   Convert a list of chars into a string.
+*)
+let combine lc = combine_str (List.map (String.make 1) lc)
+
+(**
+   [sequence [p1; ...; pn]] tries to apply parsers p1 to pn sequentially.
+   The resulting parser returns an optional list.
+*)
+let rec sequence =
   function
   | [] -> pone
   | p::pl ->
-  begin
     let* x = p in
     let* xs = sequence pl in
-    P(fun inp -> Some(x::xs, inp))
-  end
+    P (fun inp -> Some(x::xs, inp))
 
 let sequence_of_chars cl = sequence (List.map pchar cl)
 let sequence_of_string s = sequence_of_chars (explode s)
 let literal s = fmap combine (sequence_of_string s)
 
+(** [parse f] is [P (fun inp -> f inp)] *)
 let parser f = P (fun inp -> f inp)
