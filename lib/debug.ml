f2117946a6b6d43d3f97558207d95f1e5db1043c
Arthur Correnson <arthur.correnson@gmail.com>
2020-02-23 23:05:44 +0100

    Add functions to compute interference matrix

--- lib/debug.ml
+++ lib/debug.ml
@@ -1,7 +1,27 @@
 
 open Mfc
 open Mfc_generate
+open Mfc_parsing
+open Mfc_parser
+open Mfc_env
+open Mfc_quad
+open Mfc_reg_alloc
 
 let _ =
   let ic = open_in "examples/test.gen" in
-  generate ic
+  generate ic;
+  close_in ic;
+  let ic2 = open_in "examples/test.gen" in
+  let r = read_all ic2 in
+  match parse _prog r with
+  | Some (ast, "") ->
+    let env = new_env () in
+    push_frame env;
+    new_function env "print" 0 1;
+    let ql = quad_s ast env in
+    let rc = env.label_counter+1 in
+    let arr = get_lifes ql rc in
+    let p = fun s -> IntSet.iter (fun i -> print_int i; print_char ' ') s; print_newline () in
+    Array.iter p arr;
+    inter_mat arr
+  | _ -> failwith "parse error"
---
+++ lib/mfc_reg_alloc.ml
@@ -0,0 +1,56 @@
+open Mfc_quad
+open Mfc_env
+module IntSet = Set.Make(Int)
+
+(** Compute lifes of each virtual register *)
+let get_lifes ql rc =
+  (* Array of Integer Set (life) *)
+  (* lifes[i] is the set of instructions using i *)
+  let lifes = Array.make rc (IntSet.empty) in
+  let save1 i r =
+    let r' = IdType.reg_to_int r in
+    lifes.(r') <- IntSet.add i lifes.(r');
+  in
+  let save2 i r1 r2 =
+    let r1' = IdType.reg_to_int r1 in
+    let r2' = IdType.reg_to_int r2 in
+    lifes.(r1') <- IntSet.add i lifes.(r1');
+    lifes.(r2') <- IntSet.add i lifes.(r2');
+  in
+  let save3 i r1 r2 r3 =
+    let r1' = IdType.reg_to_int r1 in
+    let r2' = IdType.reg_to_int r2 in
+    let r3' = IdType.reg_to_int r3 in
+    lifes.(r1') <- IntSet.add i lifes.(r1');
+    lifes.(r2') <- IntSet.add i lifes.(r2');
+    lifes.(r3') <- IntSet.add i lifes.(r3');
+  in
+  let update i q =
+    match q with
+    | Q_BINOP (_, r1, r2, r3) -> save3 i r1 r2 r3
+    | Q_BINOPI (_, r1, r2, _) -> save2 i r1 r2
+    | Q_CMP (r1, r2) -> save2 i r1 r2
+    | Q_IFP (r1, _) -> save1 i r1
+    | Q_LD (r1, r2) -> save2 i r1 r2
+    | Q_POP (r1) -> save1 i r1
+    | Q_PUSH (r1) -> save1 i r1
+    | Q_SET (r1, r2) -> save2 i r1 r2
+    | Q_SETI (r1, _) -> save1 i r1
+    | Q_STR (r1, r2) -> save2 i r1 r2
+    | Q_UNOP (_, r1, r2) -> save2 i r1 r2
+    | _ -> ()
+  in
+  List.iteri update ql;
+  lifes
+
+let inter_mat arr =
+  let len = Array.length arr in
+  let mat = Array.make_matrix len len false in
+  for i = 0 to len - 1 do
+    for j = 0 to len - 1 do
+      let inter = IntSet.inter arr.(i) arr.(j) in
+      if i <> j && inter = IntSet.empty 
+      then mat.(i).(j) <- true
+    done
+  done;
+  mat
